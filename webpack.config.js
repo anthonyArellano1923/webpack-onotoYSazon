3262d2cec3903c5ec87d3d061e363e01d037236b
anthonyArellano1923 <163557083+anthonyArellano1923@users.noreply.github.com>
2025-07-08 10:44:44 -0400

    Agrega style-loader y ajusta configuración de webpack para manejo de estilos y recursos

--- webpack.config.js
+++ webpack.config.js
@@ -9,40 +9,49 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
+  mode,
   entry: './src/js/index.js',
+
   output: {
     filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: './',
+    publicPath: mode === 'production' ? './' : '/',
     clean: true,
   },
+
+  devtool: mode === 'development' ? 'inline-source-map' : false,
+
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: [
           mode === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
-          'css-loader'
+          'css-loader',
         ],
-      }
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
+
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
     }),
     new MiniCssExtractPlugin({
       filename: '[name].[contenthash].css',
-      
-    })
+    }),
   ],
+
   devServer: {
     static: {
-      directory: path.resolve(__dirname, 'dist'),
+      directory: path.resolve(__dirname, 'public'),
     },
     port: 8080,
     open: true,
-    hot: true
+    hot: true,
   },
-  mode: 'development',
 };
