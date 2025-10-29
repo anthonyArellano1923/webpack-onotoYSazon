c2ac30b326c31c912ab9e1c61a66bfc4f59c9f82
anthonyArellano1923 <163557083+anthonyArellano1923@users.noreply.github.com>
2025-10-29 08:22:57 -0300

    feat: mejora en la visualización de tarjetas sociales y actualización de íconos en el JSON

--- src/components/SocialCards.js
+++ src/components/SocialCards.js
@@ -4,9 +4,9 @@ const SocialCards = async function () {
   const socials = await getSocials()
   const view = socials.map(social => `
     <div style="background-color: ${social.backgroundColor};" class="contact-card__social-media__item">
-      <a href="${social.url}>
-        ${social.name}
-        <img src="${social.icon}" alt="${social.alt}">
+      <a class="" href="${social.url}" target="_blank" rel="noopener noreferrer" aria-label="${social.name}">
+      <img src="${social.icon}" alt="${social.alt || social.name}">  
+      <p>${social.name}</p>
       </a>
     </div>
     `).join('')
